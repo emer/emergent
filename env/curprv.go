87d339845930cc8741ea7020f594c28c47e74427
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-10-20 12:08:25 -0700

    add CurPrvInt  and F32 explicit type for CurPrv

--- env/curprv.go
+++ env/curprv.go
@@ -4,19 +4,36 @@
 
 package env
 
-// CurPrv is basic state management for current and previous values
-type CurPrv struct {
+// CurPrvF32 is basic state management for current and previous values, float32 values
+type CurPrvF32 struct {
 	Cur float32 `desc:"current value"`
 	Prv float32 `desc:"previous value"`
 }
 
 // Update updates the new current value, copying Cur to Prv
-func (cv *CurPrv) Update(cur float32) {
+func (cv *CurPrvF32) Update(cur float32) {
 	cv.Prv = cv.Cur
 	cv.Cur = cur
 }
 
 // Diff returns the difference between current and previous values
-func (cv *CurPrv) Diff() float32 {
+func (cv *CurPrvF32) Diff() float32 {
+	return cv.Cur - cv.Prv
+}
+
+// CurPrvInt is basic state management for current and previous values, int values
+type CurPrvInt struct {
+	Cur int `desc:"current value"`
+	Prv int `desc:"previous value"`
+}
+
+// Update updates the new current value, copying Cur to Prv
+func (cv *CurPrvInt) Update(cur int) {
+	cv.Prv = cv.Cur
+	cv.Cur = cur
+}
+
+// Diff returns the difference between current and previous values
+func (cv *CurPrvInt) Diff() int {
 	return cv.Cur - cv.Prv
 }
