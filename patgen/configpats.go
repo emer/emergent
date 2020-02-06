8a8bf73650be3d9fbe4effbf9cdeb860f4a08163
zycyc <alanzycyc@gmail.com>
2020-02-06 13:44:43 -0800

    update configvocab and configpats with test

--- patgen/configpats.go
+++ patgen/configpats.go
@@ -23,103 +23,29 @@ func InitPats(dt *etable.Table, name, desc, inputName, outputName string, listSi
 	}, listSize)
 }
 
-// ConfigPatsTrain configures training patterns using first listSize rows in the vocabulary list
-// design order: left right, bottom up
-func ConfigPatsTrain(dt, poolVocab *etable.Table, design []string) {
+// ConfigPats configures patterns using first listSize rows in the vocabulary map
+// poolSource order: left right, bottom up
+func ConfigPats(dt *etable.Table, mp map[string]*etensor.Float32, colName string, poolSource []string) error {
 	name := dt.MetaData["name"]
-	inputName := dt.ColName(1)
-	outputName := dt.ColName(2)
-	listSize := dt.ColByName(inputName).Shapes()[0]
-	ySize := dt.ColByName(inputName).Shapes()[1]
-	xSize := dt.ColByName(inputName).Shapes()[2]
+	listSize := dt.ColByName(colName).Shapes()[0]
+	ySize := dt.ColByName(colName).Shapes()[1]
+	xSize := dt.ColByName(colName).Shapes()[2]
 	for row := 0; row < listSize; row++ {
 		dt.CellTensor("Name", row).SetString([]int{0}, fmt.Sprint(name, row))
 		npool := 0
-		for pooly := 0; pooly < ySize; pooly++ {
-			for poolx := 0; poolx < xSize; poolx++ {
-				tmpInp := dt.CellTensor(inputName, row).SubSpace([]int{pooly, poolx})
-				tmpOut := dt.CellTensor(outputName, row).SubSpace([]int{pooly, poolx})
-				tmpInp.CopyFrom(poolVocab.CellTensor(design[npool], row))
-				tmpOut.CopyFrom(tmpInp)
-				npool++
-			}
-		}
-	}
-}
-
-// ConfigPatsTest configures testing patterns using training patterns
-// voidDesign: bool slices to decide which pools will be turned to void in Input
-func ConfigPatsTest(dt, trainDT *etable.Table, voidDesign []bool) error {
-	inputName := dt.ColName(1)
-	outputName := dt.ColName(2)
-	testShapes := dt.ColByName(inputName).Shapes()
-	trainShapes := trainDT.ColByName(inputName).Shapes()
-
-	// check shape
-	fmt.Println(testShapes)
-	fmt.Println(trainShapes)
-	if !reflect.DeepEqual(testShapes, trainShapes) {
-		fmt.Println("hit an error but not terminating?")
-		err := fmt.Errorf("Train and Test should have the same shape") // how do I stop the program?
-		fmt.Println(err.Error())
-	}
-
-	name := dt.MetaData["name"]
-	listSize := testShapes[0]
-	ySize := testShapes[1]
-	xSize := testShapes[2]
-	for row := 0; row < listSize; row++ {
-		dt.CellTensor("Name", row).SetString([]int{0}, fmt.Sprint(name, row))
-		npool := 0
-		for pooly := 0; pooly < ySize; pooly++ {
-			for poolx := 0; poolx < xSize; poolx++ {
-				tmpInp := dt.CellTensor(inputName, row).SubSpace([]int{pooly, poolx})
-				tmpOut := dt.CellTensor(outputName, row).SubSpace([]int{pooly, poolx})
-				tmpInp.CopyFrom(trainDT.CellTensor(inputName, row).SubSpace([]int{pooly, poolx}))
-				tmpOut.CopyFrom(trainDT.CellTensor(outputName, row).SubSpace([]int{pooly, poolx}))
-				if voidDesign[npool] {
-					tmpInp.SetZeros()
+		for iY := 0; iY < ySize; iY++ {
+			for iX := 0; iX < xSize; iX++ {
+				pool := dt.CellTensor(colName, row).SubSpace([]int{iY, iX})
+				frmPool := mp[poolSource[npool]].SubSpace([]int{row})
+				if !reflect.DeepEqual(pool.Shapes(), frmPool.Shapes()) {
+					err := fmt.Errorf("Vocab and pools in the table should have the same shape") // how do I stop the program?
+					// fmt.Println(err.Error())
+					return err
 				}
+				pool.CopyFrom(frmPool)
 				npool++
 			}
 		}
 	}
 	return nil
 }
-
-// PatsFlipBits flips nFlipBits bits in pools specified by flipDesign
-func PatsFlipBits(dt *etable.Table, flipDesign []bool, nFlipBits int) {
-	inputName := dt.ColName(1)
-	outputName := dt.ColName(2)
-	dtShapes := dt.ColByName(inputName).Shapes()
-	listSize := dtShapes[0]
-	ySize := dtShapes[1]
-	xSize := dtShapes[2]
-	for row := 0; row < listSize; row++ {
-		npool := 0
-		for pooly := 0; pooly < ySize; pooly++ {
-			for poolx := 0; poolx < xSize; poolx++ {
-				tmpInp := dt.CellTensor(inputName, row).SubSpace([]int{pooly, poolx})
-				tmpOut := dt.CellTensor(outputName, row).SubSpace([]int{pooly, poolx})
-				if flipDesign[npool] {
-					FlipBits(tmpInp, nFlipBits, nFlipBits, 1, 0)
-					tmpOut.CopyFrom(tmpInp)
-				}
-				npool++
-			}
-		}
-	}
-}
-
-// Example codes for TrainAB and TestAB in Hip.go
-// ss.ConfigVocab(ss.PoolVocab, ss.ECPool.Y, ss.ECPool.X, ss.ECPctAct, []string{"A", "B", "ctxt1a", "ctxt1b", "ctxt1c", "ctxt1d"})
-// for _, poolname := range []string{"ctxt1a", "ctxt1b", "ctxt1c", "ctxt1d"} { // the context for A-B should remain the same
-// 	for _, row := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9} { // thus we copy the tensors from row1 to row9 from row0
-// 		ss.PoolVocab.CellTensor(poolname, row).CopyFrom(ss.PoolVocab.CellTensor(poolname, 0))
-// 	}
-// }
-// patgen.InitPats(ss.TrainAB, "TrainAB", "AB Training Patterns", "Input", "ECout", 10, ss.YSize, ss.XSize, ss.ECPool.Y, ss.ECPool.X)
-// patgen.InitPats(ss.TestAB, "TestAB", "AB Testing Patterns", "Input", "ECout", 10, ss.YSize, ss.XSize, ss.ECPool.Y, ss.ECPool.X)
-// patgen.ConfigPatsTrain(ss.TrainAB, ss.PoolVocab, []string{"A", "B", "ctxt1a", "ctxt1b", "ctxt1c", "ctxt1d"})
-// patgen.PatsFlipBits(ss.TrainAB, []bool{false, false, true, true, true, true}, 1) // bool slices to decide which pool to flip
-// patgen.ConfigPatsTest(ss.TestAB, ss.TrainAB, []bool{false, true, false, false, false, false}) // bool slices to decide which pool to zero in Input
--- patgen/configvocab.go
+++ patgen/configvocab.go
@@ -5,21 +5,138 @@
 package patgen
 
 import (
-	"github.com/emer/etable/etable"
+	"fmt"
+	"math"
+	"math/rand"
+
 	"github.com/emer/etable/etensor"
 )
 
-// ConfigVocab configures enough trials of vocabularies for ConfigPats
-// There's always a "void" column where the patterns are zeros
-// Index columns by pool names instead of numbers
-func ConfigVocab(dt *etable.Table, poolY, poolX int, ecPctAct float32, poolNames []string) {
-	rows := 1000
-	nOn := int(float32(poolY) * float32(poolX) * ecPctAct)
-	dt.SetFromSchema(etable.Schema{
-		{"void", etensor.FLOAT32, []int{poolY, poolX}, []string{"Y", "X"}},
-	}, rows)
-	for i, name := range poolNames {
-		dt.AddCol(dt.Cols[0].Clone(), name)
-		PermutedBinaryRows(dt.Cols[i+1], nOn, 1, 0)
+// AddVocabVoid adds a void pool to the vocabulary
+func AddVocabVoid(mp map[string]*etensor.Float32, rows, poolY, poolX int, poolName string) {
+	tsr := etensor.NewFloat32([]int{rows, poolY, poolX}, nil, []string{"row", "Y", "X"})
+	mp[poolName] = tsr
+}
+
+// AddVocab adds a normal pool to the vocabulary
+func AddVocab(mp map[string]*etensor.Float32, rows, poolY, poolX int, pctAct float32, poolName string) {
+	nOn := int(math.Round(float64(poolY) * float64(poolX) * float64(pctAct)))
+	tsr := etensor.NewFloat32([]int{rows, poolY, poolX}, nil, []string{"row", "Y", "X"})
+	PermutedBinaryRows(tsr, nOn, 1, 0)
+	mp[poolName] = tsr
+}
+
+// AddVocabRepeat adds a repeated pool to the vocabulary
+func AddVocabRepeat(mp map[string]*etensor.Float32, rows, poolY, poolX int, pctAct float32, poolName string) {
+	nOn := int(math.Round(float64(poolY) * float64(poolX) * float64(pctAct)))
+	tsr := etensor.NewFloat32([]int{rows, poolY, poolX}, nil, []string{"row", "Y", "X"})
+	for i := 0; i < rows; i++ {
+		subtsr := tsr.SubSpace([]int{i})
+		if i == 0 {
+			PermutedBinary(subtsr, nOn, 1, 0)
+		} else {
+			subtsr.CopyFrom(tsr.SubSpace([]int{i - 1}))
+		}
 	}
+	mp[poolName] = tsr
+}
+
+// AddVocabDrift adds a temporally drifted pool to the vocabulary
+func AddVocabDrift(mp map[string]*etensor.Float32, rows, poolY, poolX int, pctAct, pctDrift float32, poolName string) {
+	nOn := int(math.Round(float64(poolY) * float64(poolX) * float64(pctAct)))
+	nDrift := int(math.Round(float64(nOn) * float64(pctDrift)))
+	AddVocab(mp, rows, poolY, poolX, pctAct, poolName)
+	VocabDrift(mp, nDrift, poolName)
+}
+
+// VocabDrift makes a pool in the vocabulary temporally drifted
+func VocabDrift(mp map[string]*etensor.Float32, nDrift int, poolName string) {
+	tsr := mp[poolName]
+	rows := tsr.Shapes()[0]
+	for i := 0; i < rows; i++ {
+		subtsr := tsr.SubSpace([]int{i})
+		if i > 0 {
+			subtsr.CopyFrom(tsr.SubSpace([]int{i - 1}))
+			FlipBits(subtsr, nDrift, nDrift, 1, 0)
+		}
+	}
+}
+
+// VocabClone clones an old pool in the vocabulary and make it a new one
+func VocabClone(mp map[string]*etensor.Float32, frmPool, toNewPool string) {
+	mp[toNewPool] = mp[frmPool].Clone().(*etensor.Float32)
+}
+
+// VocabShuffle shuffles a pool in the vocabulary on its first dimension (row)
+func VocabShuffle(mp map[string]*etensor.Float32, shuffleKeys []string) {
+	for _, shuffleKey := range shuffleKeys {
+		tsr := mp[shuffleKey]
+		rows := tsr.Shapes()[0]
+		poolY := tsr.Shapes()[1]
+		poolX := tsr.Shapes()[2]
+		sRows := rand.Perm(rows)
+		sTsr := etensor.NewFloat32([]int{rows, poolY, poolX}, nil, []string{"row", "Y", "X"})
+		for iRow, sRow := range sRows {
+			sTsr.SubSpace([]int{iRow}).CopyFrom(tsr.SubSpace([]int{sRow}))
+		}
+		mp[shuffleKey] = sTsr
+	}
+}
+
+// VocabConcat contatenates several pools in the vocabulary and make it a new one
+func VocabConcat(mp map[string]*etensor.Float32, newKey string, frmKeys []string) {
+	tsr := mp[frmKeys[0]].Clone().(*etensor.Float32)
+	for i, key := range frmKeys {
+		// check dimension, how
+		if i > 0 {
+			currows := tsr.Shapes()[0]
+			approws := mp[key].Shapes()[0]
+			tsr.SetShape([]int{currows + approws, tsr.Shapes()[1], tsr.Shapes()[2]}, nil, []string{"row", "Y", "X"})
+			for iRow := 0; iRow < approws; iRow++ {
+				subtsr := tsr.SubSpace([]int{iRow + currows})
+				subtsr.CopyFrom(mp[key].SubSpace([]int{iRow}))
+			}
+		}
+	}
+	mp[newKey] = tsr
+}
+
+// VocabSlice slices a pool in the vocabulary into new ones
+func VocabSlice(mp map[string]*etensor.Float32, frmKey string, newKeys []string, sliceOffs []int) error {
+	oriTsr := mp[frmKey]
+	poolY := oriTsr.Shapes()[1]
+	poolX := oriTsr.Shapes()[2]
+
+	// check newKeys and sliceOffs have same length
+	if len(newKeys)+1 != len(sliceOffs) {
+		err := fmt.Errorf("sliceOffs should have one more element than newKeys") // how do I stop the program?
+		fmt.Println(err.Error())
+	}
+
+	// check sliceOffs is in right order
+	preVal := sliceOffs[0]
+	for i, curVal := range sliceOffs {
+		if i > 0 {
+			if preVal < curVal {
+				preVal = curVal
+			} else {
+				err := fmt.Errorf("sliceOffs should increase progressively") // how do I stop the program?
+				fmt.Println(err.Error())
+			}
+		}
+	}
+
+	// slice
+	frmOff := sliceOffs[0]
+	for i := range newKeys {
+		toOff := sliceOffs[i+1]
+		newKey := newKeys[i]
+		newTsr := etensor.NewFloat32([]int{toOff - frmOff, poolY, poolX}, nil, []string{"row", "Y", "X"})
+		for off := frmOff; off < toOff; off++ {
+			newTsr.SubSpace([]int{off - frmOff}).CopyFrom(oriTsr.SubSpace([]int{off}))
+		}
+		mp[newKey] = newTsr
+		frmOff = toOff
+	}
+	return nil
 }
