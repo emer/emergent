6d9c12c7b748bcffe26477d9b61e6bf90201cd61
zycyc <alanzycyc@gmail.com>
2020-02-18 22:59:23 -0800

    update readme; add colIndependent into Shuffle

--- patgen/configvocabpats_test.go
+++ patgen/configvocabpats_test.go
@@ -12,12 +12,12 @@ import (
 func TestVocab(t *testing.T) {
 	fmt.Println("Testing starts")
 	m := make(map[string]*etensor.Float32)
-	AddVocabEmpty(m, "empty", 3, 3, 3)
-	AddVocabPermutedBinary(m, "A", 3, 3, 3, 0.3, 0.5)
-	AddVocabDrift(m, "B", 3, 0.2, "A", 0) // nOn=4*(3*3*0.3); nDrift=nOn*0.5
-	AddVocabRepeat(m, "ctxt1", 3, "A", 0)
+	AddVocabEmpty(m, "empty", 6, 3, 3)
+	AddVocabPermutedBinary(m, "A", 6, 3, 3, 0.3, 0.5)
+	AddVocabDrift(m, "B", 6, 0.2, "A", 0) // nOn=4*(3*3*0.3); nDrift=nOn*0.5
+	AddVocabRepeat(m, "ctxt1", 6, "A", 0)
 	VocabConcat(m, "AB-C", []string{"A", "B"})
-	VocabSlice(m, "AB-C", []string{"A'", "B'"}, []int{0, 3, 6}) // 3 cutoffs for 2 vocabs
+	VocabSlice(m, "AB-C", []string{"A'", "B'"}, []int{0, 6, 12}) // 3 cutoffs for 2 vocabs
 	VocabShuffle(m, []string{"B'"})
 	AddVocabClone(m, "B''", "B'")
 
@@ -50,10 +50,18 @@ func TestVocab(t *testing.T) {
 
 	// config pats
 	dt := etable.NewTable("TrainAB")
-	InitPats(dt, "TrainAB", "describe", "Input", "ECout", 3, 3, 2, 3, 3)
+	InitPats(dt, "TrainAB", "describe", "Input", "ECout", 6, 3, 2, 3, 3)
 	MixPats(dt, m, "Input", []string{"A", "B", "ctxt1", "ctxt1", "empty", "B'"})
+	MixPats(dt, m, "ECout", []string{"A", "B", "ctxt1", "ctxt1", "empty", "B'"})
 
-	fmt.Println("Pats'")
+	// try shuffle
+	Shuffle(dt, []int{0, 1, 2, 3, 4, 5}, []string{"Input", "ECout"}, false)
+
+	fmt.Println("Input Pats")
 	fmt.Println(dt.ColByName("Input").Shapes())
 	fmt.Println(dt.ColByName("Input").T())
+
+	fmt.Println("ECout Pats")
+	fmt.Println(dt.ColByName("ECout").Shapes())
+	fmt.Println(dt.ColByName("ECout").T())
 }
--- patgen/shuffle.go
+++ patgen/shuffle.go
@@ -12,16 +12,30 @@ import (
 )
 
 // Shuffle shuffles rows in specified columns in the table independently
-func Shuffle(dt *etable.Table, rows []int, colNames []string) {
+func Shuffle(dt *etable.Table, rows []int, colNames []string, colIndependent bool) {
 	cl := dt.Clone()
-	for _, colNm := range colNames {
-		orirows := make([]int, len(rows))
-		copy(orirows, rows)
-		erand.PermuteInts(rows)
-		fmt.Println(orirows)
+	if colIndependent { // independent
+		for _, colNm := range colNames {
+			sfrows := make([]int, len(rows))
+			copy(sfrows, rows)
+			erand.PermuteInts(sfrows)
+			fmt.Println(sfrows)
+			fmt.Println(rows)
+			for i, row := range rows {
+				dt.CellTensor(colNm, row).CopyFrom(cl.CellTensor(colNm, sfrows[i]))
+			}
+		}
+	} else { // shuffle together
+		sfrows := make([]int, len(rows))
+		copy(sfrows, rows)
+		erand.PermuteInts(sfrows)
+		fmt.Println(sfrows)
 		fmt.Println(rows)
-		for i, row := range rows {
-			dt.CellTensor(colNm, row).CopyFrom(cl.CellTensor(colNm, orirows[i]))
+		for _, colNm := range colNames {
+			for i, row := range rows {
+				dt.CellTensor(colNm, row).CopyFrom(cl.CellTensor(colNm, sfrows[i]))
+			}
 		}
 	}
+
 }
