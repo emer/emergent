3b8dc87c00c46aa0ad3d4e0cb7235327cf43970b
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2021-04-17 12:59:37 -0700

    resolved version of #73

--- patgen/configvocab.go
+++ patgen/configvocab.go
@@ -129,13 +129,13 @@ func AddVocabDrift(mp Vocab, name string, rows int, pctDrift float32, copyFrom s
 	trow := tsr.SubSpace([]int{0})
 	trow.CopyFrom(cprow)
 	nOn := NOnInTensor(cprow)
-	rmdr := 0.0                               // remainder carryover in drift, JWA new re: ROR
-	drift := float64(nOn) * float64(pctDrift) // precise fractional amount of drift, JWA new re: ROR
+	rmdr := 0.0                               // remainder carryover in drift
+	drift := float64(nOn) * float64(pctDrift) // precise fractional amount of drift
 	for i := 1; i < rows; i++ {
 		srow := tsr.SubSpace([]int{i - 1})
 		trow := tsr.SubSpace([]int{i})
 		trow.CopyFrom(srow)
-		curDrift := math.Round(float64(nOn) * (float64(pctDrift) + rmdr)) // integer amount
+		curDrift := math.Round(drift + rmdr) // integer amount
 		nDrift := int(curDrift)
 		if nDrift > 0 {
 			FlipBits(trow, nDrift, nDrift, 1, 0)
