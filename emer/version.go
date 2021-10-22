f533e604942e6578f91de3eb5e439da14c954de8
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2021-10-22 02:39:15 -0700

    v1.1.39 release -- version.go updated

--- emer/version.go
+++ emer/version.go
@@ -3,7 +3,7 @@
 package emer
 
 const (
-	Version     = "v1.1.38"
-	GitCommit   = "97d6653"          // the commit JUST BEFORE the release
-	VersionDate = "2021-09-03 07:26" // UTC
+	Version     = "v1.1.39"
+	GitCommit   = "b9d91d5"          // the commit JUST BEFORE the release
+	VersionDate = "2021-10-22 09:39" // UTC
 )
