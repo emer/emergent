cbf7b96221f095e036daef3a4c46252409a7f935
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-01-06 00:40:42 -0800

    diffuse doesn't divide by volume in deltas -- validated in urakubo model

--- chem/diffuse.go
+++ chem/diffuse.go
@@ -6,8 +6,8 @@ package chem
 
 // Diffuse models diffusion between two compartments A and B as
 // a function of concentration in each and potentially asymmetric
-// rate constants --  A Kf -> B and B Kb -> A
-// computes the difference and applies to each
+// rate constants: A Kf -> B and B Kb -> A
+// computes the difference between each direction and applies to each
 type Diffuse struct {
 	Kf float64 `desc:"A -> B forward diffusion rate constant, sec-1"`
 	Kb float64 `desc:"B -> A backward diffusion rate constant, sec-1"`
@@ -26,10 +26,9 @@ func (rt *Diffuse) SetSym(kfb float64) {
 }
 
 // Step computes delta A and B values based on current A, B values
-// inputs are numbers, converted to concentration, then back to numbers
-// for the deltas.
+// inputs are numbers, converted to concentration to drive rate
 func (rt *Diffuse) Step(ca, cb, va, vb float64, da, db *float64) {
 	df := rt.Kf*(ca/va) - rt.Kb*(cb/vb)
-	*da -= df * va
-	*db += df * vb
+	*da -= df
+	*db += df
 }
