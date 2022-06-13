740c5361bea66e1d5eb5f10a390e06220fcfe752
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2022-06-12 20:18:46 -0700

    v1.3.13 release -- version.go updated

--- emer/version.go
+++ emer/version.go
@@ -3,7 +3,7 @@
 package emer
 
 const (
-	Version     = "v1.3.12"
-	GitCommit   = "40a1ba0"          // the commit JUST BEFORE the release
-	VersionDate = "2022-06-13 00:32" // UTC
+	Version     = "v1.3.13"
+	GitCommit   = "d5b1627"          // the commit JUST BEFORE the release
+	VersionDate = "2022-06-13 03:18" // UTC
 )
