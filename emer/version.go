df2f43d1db552e27a267a29bc0d1a042404b4342
Randall C. O'Reilly <oreilly@ucdavis.edu>
2023-06-16 09:18:31 -0700

    v1.4.9 release -- version.go updated

--- emer/version.go
+++ emer/version.go
@@ -3,7 +3,7 @@
 package emer
 
 const (
-	Version     = "v1.4.8"
-	GitCommit   = "955492e"          // the commit JUST BEFORE the release
-	VersionDate = "2023-06-09 22:45" // UTC
+	Version     = "v1.4.9"
+	GitCommit   = "98699e9"          // the commit JUST BEFORE the release
+	VersionDate = "2023-06-16 16:18" // UTC
 )
