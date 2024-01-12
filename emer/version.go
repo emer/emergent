7b4bada569704d31ad1c5bb5bf6a36befc3ee3a6
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-01-12 08:57:27 -0800

    updated version to v2.0.0-dev0.0.6

--- emer/version.go
+++ emer/version.go
@@ -4,9 +4,9 @@ package emer
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.5"
+	Version = "v2.0.0-dev0.0.6"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "68f9509"
+	GitCommit = "bca6ff5"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2024-01-12 16:54"
+	VersionDate = "2024-01-12 16:57"
 )
