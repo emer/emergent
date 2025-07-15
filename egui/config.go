87f66106d447a0f3adad3b8a69cdb018974ade8d
Kai O'Reilly <koreilly5297@gmail.com>
2025-07-15 16:12:52 -0700

    egui: add Config, BaseConfig

---
+++ egui/config.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2025, The Emergent Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package egui
+
+// Config is an interface implemented by all sim config types.
+// To implement Config, you must embed [BaseConfig].
+type Config interface {
+
+	// AsBaseConfig returns the embedded [BaseConfig].
+	AsBaseConfig() *BaseConfig
+}
+
+// BaseConfig contains the basic configuration parameters common to all sims.
+type BaseConfig struct {
+
+	// Name is the short name of the sim.
+	Name string `display:"-"`
+
+	// Title is the longer title of the sim.
+	Title string `display:"-"`
+
+	// URL is a link to the online README or other documentation for this sim.
+	URL string `display:"-"`
+
+	// Doc is brief documentation of the sim.
+	Doc string `display:"-"`
+
+	// Includes has a list of additional config files to include.
+	// After configuration, it contains list of include files added.
+	Includes []string
+
+	// GUI indicates to open the GUI. Otherwise it runs automatically and quits,
+	// saving results to log files.
+	GUI bool `default:"true"`
+
+	// Debug indicates to report debugging information.
+	Debug bool
+}
+
+func (bc *BaseConfig) AsBaseConfig() *BaseConfig { return bc }
+
+func (bc *BaseConfig) IncludesPtr() *[]string { return &bc.Includes }
