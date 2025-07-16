ff8c75593d2d9b7cfde8bc2f72d204ed6121da0e
Kai O'Reilly <koreilly5297@gmail.com>
2025-07-15 17:10:25 -0700

    egui: move Run to sim.go

--- egui/config.go
+++ egui/config.go
@@ -4,9 +4,7 @@
 
 package egui
 
-import "cogentcore.org/core/cli"
-
-// Config is an interface implemented by all sim config types.
+// Config is an interface implemented by all [Sim] config types.
 // To implement Config, you must embed [BaseConfig]. You must
 // implement [Config.Defaults] yourself.
 type Config interface {
@@ -48,29 +46,3 @@ type BaseConfig struct {
 func (bc *BaseConfig) AsBaseConfig() *BaseConfig { return bc }
 
 func (bc *BaseConfig) IncludesPtr() *[]string { return &bc.Includes }
-
-//////////// Run
-
-// Run runs a sim using the given function that runs the sim.
-// This is a high-level helper function designed to be called as one-liner
-// from the main() function of the sim's command subdirectory with package main.
-//
-// This subdirectory has the same name as the sim name itself, ex: sims/ra25
-// has the package with the sim logic, and sims/ra25/ra25 has the compilable main().
-//
-// Run uses the config type C determined from the runSim function to make a new
-// [Config] object and set its default values with [Config.Defaults]. The given runSim
-// function MUST take a single argument that is a pointer to the [Config] type for the
-// sim. If its argument type does not implement [Config], Run will panic.
-func Run[C any](runSim func(cfg *C) error) {
-	cfgC := new(C)
-	cfg := any(cfgC).(Config)
-	cfg.Defaults()
-
-	bc := cfg.AsBaseConfig()
-	opts := cli.DefaultOptions(bc.Name, bc.Title)
-	opts.DefaultFiles = append(opts.DefaultFiles, "config.toml")
-	opts.SearchUp = true // so that the sim can be run from the command subdirectory
-
-	cli.Run(opts, cfgC, runSim)
-}
--- egui/sim.go
+++ egui/sim.go
@@ -5,6 +5,7 @@
 package egui
 
 import (
+	"cogentcore.org/core/cli"
 	"cogentcore.org/core/core"
 	"cogentcore.org/core/tree"
 )
@@ -24,6 +25,30 @@ type Sim[C any] interface {
 	Body() *core.Body
 
 	RunNoGUI()
+}
+
+// Run runs a sim using the given function that runs the sim.
+// This is a high-level helper function designed to be called as one-liner
+// from the main() function of the sim's command subdirectory with package main.
+//
+// This subdirectory has the same name as the sim name itself, ex: sims/ra25
+// has the package with the sim logic, and sims/ra25/ra25 has the compilable main().
+//
+// Run uses the config type C determined from the runSim function to make a new
+// [Config] object and set its default values with [Config.Defaults]. The given runSim
+// function MUST take a single argument that is a pointer to the [Config] type for the
+// sim. If its argument type does not implement [Config], Run will panic.
+func Run[C any](runSim func(cfg *C) error) {
+	cfgC := new(C)
+	cfg := any(cfgC).(Config)
+	cfg.Defaults()
+
+	bc := cfg.AsBaseConfig()
+	opts := cli.DefaultOptions(bc.Name, bc.Title)
+	opts.DefaultFiles = append(opts.DefaultFiles, "config.toml")
+	opts.SearchUp = true // so that the sim can be run from the command subdirectory
+
+	cli.Run(opts, cfgC, runSim)
 }
 
 // RunSim runs a sim with the given config. *S must implement [Sim][C]
