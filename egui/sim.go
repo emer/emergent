af8b168ea3db47949d568746511e705e45dc618c
Kai O'Reilly <koreilly5297@gmail.com>
2025-07-15 17:07:23 -0700

    egui: add RunSim

--- egui/sim.go
+++ egui/sim.go
@@ -4,7 +4,10 @@
 
 package egui
 
-import "cogentcore.org/core/core"
+import (
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/tree"
+)
 
 // Sim is an interface implemented by all sim types.
 // It is parameterized by the config type C. *C must implement [Config].
@@ -15,10 +18,34 @@ type Sim[C any] interface {
 
 	ConfigSim()
 	Init()
-	ConfigGUI()
+	ConfigGUI(b tree.Node)
 
 	// Body returns the [core.Body] used by the sim.
 	Body() *core.Body
 
 	RunNoGUI()
+}
+
+// RunSim runs a sim with the given config. *S must implement [Sim][C]
+// (interface [Sim] parameterized by config type C).
+//
+// Unlike [Run], this does not handle command-line config parsing. End users
+// should typically use [Run], which uses RunSim under the hood.
+func RunSim[S, C any](cfg *C) error {
+	simS := new(S)
+	sim := any(simS).(Sim[C])
+
+	bc := any(cfg).(Config).AsBaseConfig()
+
+	sim.SetConfig(cfg)
+	sim.ConfigSim()
+
+	if bc.GUI {
+		sim.Init()
+		sim.ConfigGUI(nil)
+		sim.Body().RunMainWindow()
+	} else {
+		sim.RunNoGUI()
+	}
+	return nil
 }
